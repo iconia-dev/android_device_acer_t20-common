6c51cf5609e6c9877751ec25e4693999d3febc29
Kris ~ <waaydownsouth@gmail.com>
2012-07-11 18:45:18 +1200

    only building wifi for now

--- t20-common.mk
+++ t20-common.mk
@@ -29,11 +29,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
     dalvik.vm.heapsize=256m \
     persist.sys.usb.config=mtp,adb
 
-ifneq ($(filter $(CM_BUILD),a100 a200 a500),)
-    PRODUCT_PROPERTY_OVERRIDES += ro.dinfo.radio=Wifi
-else
-    PRODUCT_PROPERTY_OVERRIDES += ro.dinfo.radio=3G
-endif
+
+PRODUCT_PROPERTY_OVERRIDES += ro.dinfo.radio=Wifi
 
 PRODUCT_CHARACTERISTICS := tablet
 
